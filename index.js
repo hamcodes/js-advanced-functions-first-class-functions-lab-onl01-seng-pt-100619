94a678ce287c4cdf91a4a5044dee286d0f9d1395
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2020-05-22 06:23:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
-function selectDifferentDrivers(array, returnFirstTwoDrivers) = {
-  return returnFirstTwoDrivers
+function selectDifferentDrivers(arr, func) {
+    return func(arr)
 }
